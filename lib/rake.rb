06b974369bb3a771c0b435f40b40740442931a78
Josh Nichols <josh@technicalpickles.com>
2009-02-20 14:55:49 -0500

    Moved Rake::NameSpace to its own file.

--- lib/rake.rb
+++ lib/rake.rb
@@ -58,6 +58,8 @@ require 'rake/multi_task'
 require 'rake/rake_file_utils'
 require 'rake/file_list'
 
+require 'rake/name_space'
+
 module Rake
 
   # Errors -----------------------------------------------------------
@@ -280,31 +282,6 @@ module Rake
 end # module Rake
 
 module Rake
-
-  ####################################################################
-  # The NameSpace class will lookup task names in the the scope
-  # defined by a +namespace+ command.
-  #
-  class NameSpace
-
-    # Create a namespace lookup object using the given task manager
-    # and the list of scopes.
-    def initialize(task_manager, scope_list)
-      @task_manager = task_manager
-      @scope = scope_list.dup
-    end
-
-    # Lookup a task named +name+ in the namespace.
-    def [](name)
-      @task_manager.lookup(name, @scope)
-    end
-
-    # Return the list of tasks defined in this and nested namespaces.
-    def tasks
-      @task_manager.tasks_in_scope(@scope)
-    end
-  end # NameSpace
-
 
   ####################################################################
   # The TaskManager module is a mixin for managing tasks.
